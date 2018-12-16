f5f21253bd3ecb44b2016e0ec130472050d594ef
Dariush Forouher <dariush@forouher.de>
2018-12-16 18:50:25 +0100

    remove los fstab

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE            := fstab.universal5420
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := fstab.universal5420
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
